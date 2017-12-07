8be195d2de0daeada7e97127f9aa31ac9b26acba
chankwen <418415881@qq.com>
2017-12-07 16:03:30 +0800

    add favicon

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import routes from './router'
-import Store from '../store/'
+import Store from '../store'
 import jwt from '../helpers/jwt'
 Vue.use(Router)
 
