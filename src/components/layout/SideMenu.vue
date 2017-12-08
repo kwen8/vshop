5990afb0766dfa71bdd43e1ff6044afec76fdc6e
chankwen <418415881@qq.com>
2017-12-08 10:37:09 +0800

    fix sideMenu

--- src/components/layout/SideMenu.vue
+++ src/components/layout/SideMenu.vue
@@ -7,18 +7,21 @@
     :active-text-color="activeTextColor"
     :collapse="collapse"
     router>
-    <side-menu-item
-      :menu-list="menuList"
-    ></side-menu-item>
+    <el-submenu v-for="menu in menuList" :key="menu.name" :index="menu.path">
+      <template slot="title">
+        <i :class="menu.icon"></i>
+        <span slot="title">{{ menu.title }}</span>
+      </template>
+      <el-menu-item v-for="subMenu in menu.children" :key="subMenu.name" :index="subMenu.path">
+        <i :class="subMenu.icon"></i>
+        {{ subMenu.title }}
+      </el-menu-item>
+    </el-submenu>
   </el-menu>
 </template>
 
 <script>
-  import sideMenuItem from './SideMenuItem'
   export default {
-    components: {
-      sideMenuItem
-    },
     props: {
       backgroundColor: {
         type: String
