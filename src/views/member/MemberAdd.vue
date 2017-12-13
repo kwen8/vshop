25ba27404bc3294ad38eefac2ac1c7c66fb4ce44
lao <562553897@qq.com>
2017-12-13 11:18:31 +0800

    会员添加

--- src/views/member/MemberAdd.vue
+++ src/views/member/MemberAdd.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    member add page
-  </div>
+  <el-card>
+    <el-row>
+      <el-col  :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+        <MemberInfoForm :buttonText="buttonText"></MemberInfoForm>
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
 
 <script>
+  import MemberInfoForm from '../../components/member/MemberInfoForm'
   export default {
+    components: {
+      MemberInfoForm
+    },
     data () {
       return {
+        buttonText: '立即创建',
         msg: 'hello vue'
       }
     }
