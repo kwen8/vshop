6a625277612c357b545114a46993d373a69fe09d
chankwen <418415881@qq.com>
2017-12-14 15:30:03 +0800

    fix axios baseURL

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import jwt from './jwt'
 
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://shop-api.dev' : 'http://shop.chenkewen.cn'
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://shop-api.dev' : 'http://shop.chenkewen.com'
 
 axios.defaults.baseURL = baseURL
 
