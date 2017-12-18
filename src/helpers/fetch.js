36a24ed1c210d7d56736646d354737737256bc9d
kwen <418415881@qq.com>
2017-12-18 16:12:25 +0800

    搭建权限管理的基础页面  api baseURl改成test后缀

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import jwt from './jwt'
 
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://shop-api.dev' : 'http://shop.chenkewen.com'
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://shop-api.test' : 'http://shop.chenkewen.com'
 
 axios.defaults.baseURL = baseURL
 
