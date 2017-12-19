b2ef4a76ae9b7adcb06e2936cb8641e8b489bcc7
lao <562553897@qq.com>
2017-12-19 14:23:57 +0800

    添加信息组件

---
+++ src/components/message/Msg.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-alert
+    :title="content.title"
+    :type="content.type"
+    :description="content.description"
+    show-icon>
+  </el-alert>
+</template>
+
+<script>
+  export default {
+    props: {
+      content: {
+        type: Object
+      }
+    },
+    components: {},
+    data () {
+      return {}
+    }
+  }
+</script>
